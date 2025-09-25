64c9c31398464375b6da50d6686965d27de845b0
cedrickeller <80917564+cedrickeller-cmd@users.noreply.github.com>
2025-09-25 19:20:23 -0400

    box-cox transformation and histograms

--- speed_dating.R
+++ speed_dating.R
@@ -1,6 +1,6 @@
 # Predictive Modeling of Speed Dating Outcomes
 
-#install.packages(c("farff", "corrplot", "caret", "e1071"))
+#install.packages(c("farff", "corrplot", "caret", "e1071", "VIM"))
 
 library(farff)
 library(mlbench)
@@ -65,15 +65,15 @@ drop_double_d = dd_cols[dd_bases %in% all_cols]
 # final drop list (bins)
 cols_bin = c(drop_single_d, drop_double_d)
 # keep everything else (continuous)
-cols_cont = setdiff(all_cols, cols_bin)
+cols_no_bin = setdiff(all_cols, cols_bin)
 
-length(cols_cont)
-length(cols_bin) ############################## #  d_d_age is duplicated somehow
-cols_cont
+length(cols_no_bin)
+length(cols_bin)-1 ############################## #  d_d_age is duplicated somehow
+cols_no_bin
 cols_bin
 
 # keep only non-bin data
-data_no_bin = data[, cols_cont]
+data_no_bin = data[, cols_no_bin]
 
 dim(data_no_bin)
 head(data_no_bin)
@@ -104,11 +104,13 @@ data_no_bin$field = case_when(
 length(table(data_no_bin$field, useNA = "ifany"))
 #table(data_no_bin$field, useNA = "ifany")
 
+
 ################################################################################
-# KNN Imputation
+# KNN Imputation   # may consider interpolation instead   # do splitting before to avoid data leakage
 ################################################################################
-data_imputed <- kNN(data_no_bin, k = 5, imp_var = FALSE)
+data_imputed = kNN(data_no_bin, k = 5, imp_var = FALSE)
 dim(data_imputed)
+colSums(is.na(data_imputed))
 
 ################################################################################
 # Separating continuous and categorical data
@@ -118,14 +120,13 @@ cat_data = data_imputed[, sapply(data_imputed, function(x) is.factor(x) | is.cha
 names(cont_data)
 names(cat_data)
 
-
 ################################################################################
 # Creating dummy variables for Categorical data
 ################################################################################
-dummies <- dummyVars(~ ., data = cat_data, fullRank=TRUE)
+dummies = dummyVars(~ ., data = cat_data, fullRank=TRUE)
 
 # Apply transformation
-cat_dummies <- data.frame(predict(dummies, newdata = cat_data))
+cat_dummies = data.frame(predict(dummies, newdata = cat_data))
 dim(cat_dummies)
 names(cat_dummies)
 
@@ -133,32 +134,147 @@ names(cat_dummies)
 # Applying correlations on Continuous Data
 ################################################################################
 
-correlations <- cor(cont_data)
-dim(correlations)
-correlations[1:4, 1:4]
-corrplot(correlations, order = "hclust")
-findCorrelation(correlations, cutoff = 0.85)
-colnames(cont_data)[c(40)]
+correlations_cont = cor(cont_data)
+dim(correlations_cont)
+#correlations_cont[1:4, 1:4]
+corrplot(correlations_cont, order = "hclust")
+findCorrelation(correlations_cont, cutoff = 0.85)
+#colnames(cont_data)[c(40)] # "museums"
+
+# columns not dropped because we will let PCA handle it
 
 ################################################################################
 # Applying NZV on Categorical Data
 ################################################################################
 
 nearZeroVar(cat_dummies)
-colnames(cat_dummies)[c(12,16)]
-cat_dummies$fieldOther
+# colnames(cat_dummies)[c(12,16)] # "fieldEcology" "fieldOther" 
+
+# columns not dropped because we will let PCA handle it
 
 
+################################################################################
+# Histograms of continuous predictors
+################################################################################
+#length(cont_data) # 59
+
+# Calculate the number of plot pages needed
+num_cols = length(names(cont_data))
+num_pages = ceiling(num_cols / 9)  # 9 plots per page (3x3 grid)
+
+# Loop through pages
+for (page in 1:num_pages) {
+  # Set up 3x3 plotting area
+  par(mfrow = c(3, 3), 
+      mar = c(4, 4, 2, 1),   # Adjust margins to give some space
+      oma = c(0, 0, 2, 0))   # Outer margin for overall title
+  
+  # Calculate start and end columns for this page
+  start_col = (page - 1) * 9 + 1
+  end_col = min(start_col + 8, num_cols)
+  
+  # Create histograms for columns on this page
+  for (i in start_col:end_col) {
+    col = names(cont_data)[i]
+    hist(cont_data[[col]], 
+         main = col, 
+         xlab = "Value",
+         col = "skyblue", 
+         border = "black")
+  }
+  
+  # Add an overall title for the page
+  mtext(paste0("Histograms (", page, "/", num_pages, ")"), outer = TRUE, cex = 1.5)
+}
+
+# Reset the plotting parameters
+par(mfrow = c(1, 1))
 
 
+################################################################################
+# Transformation -- Box Cox
+################################################################################
+
+# Get all BoxCox lambda values for predictors
+lambdas = sapply(cont_data, function(x) { 
+  BoxCoxTrans(x + 0.000001)$lambda # requires "caret" package; tiny value must be added to only have positive numbers (for the Box Cox Transformation to converge)
+})
+#lambdas
+
+skew_values = apply(cont_data, 2, skewness)
+#skewValues
+
+# Drop approximately symmetrical (=1 and >1)
+cont_skewed = names(skew_values[abs(skew_values) > 1])
+# Drop NA
+cont_skewed = lambdas[names(lambdas) != "Na"]
+
+# Box-Cox Transformed continuous list (initialization)
+cont_data_bc = cont_data
+
+# Loop through variables for transformation
+for (col in names(cont_skewed)) {
+  x = cont_data[[col]]
+  
+  # Box-Cox transform
+  bct = BoxCoxTrans(x + 0.000001)  # tiny offset to only have positive numbers (for the Box Cox Transformation to converge)
+  x_trans = predict(bct, x + 0.000001)
+  
+  # Replace original values with transformed values
+  cont_data_bc[[col]] = x_trans
+}
+
+################################################################################
+# Histograms of box-cox transformed data
+################################################################################
+#length(cont_data) # 59
+
+# Calculate the number of plot pages needed
+num_cols = length(names(cont_data_bc))
+num_pages = ceiling(num_cols / 9)  # 9 plots per page (3x3 grid)
+
+# Loop through pages
+for (page in 1:num_pages) {
+  # Set up 3x3 plotting area
+  par(mfrow = c(3, 3), 
+      mar = c(4, 4, 2, 1),   # Adjust margins to give some space
+      oma = c(0, 0, 2, 0))   # Outer margin for overall title
+  
+  # Calculate start and end columns for this page
+  start_col = (page - 1) * 9 + 1
+  end_col = min(start_col + 8, num_cols)
+  
+  # Create histograms for columns on this page
+  for (i in start_col:end_col) {
+    col = names(cont_data_bc)[i]
+    hist(cont_data_bc[[col]], 
+         main = col, 
+         xlab = "Value",
+         col = "skyblue", 
+         border = "black")
+  }
+  
+  # Add an overall title for the page
+  mtext(paste0("Histograms Box-Cox transformed (", page, "/", num_pages, ")"), outer = TRUE, cex = 1.5)
+}
+
+# Reset the plotting parameters
+par(mfrow = c(1, 1))
+
+################################################################################
+# Spatial Sign transformation
+################################################################################
+
+################################################################################
+# Box-Plot of spatial sign transformed data
+################################################################################
 
 
+################################################################################
+# PCA (incl. center + scale)
+################################################################################
 
-
-
-
-
-
+# Merge continuous + categorical (dummy var)
 
 
 
